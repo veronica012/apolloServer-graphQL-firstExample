b0d303ada7fea3451a91b39e623f959321b7a854
Veronica Celseti <veronicacelseti12@gmail.com>
2020-11-03 15:42:21 +0100

    resolver

--- index.js
+++ index.js
@@ -33,4 +33,14 @@ const books = [
       author: 'Paul Auster',
     },
   ];
+
+
+// Resolvers define the technique for fetching the types defined in the
+// schema. This resolver retrieves books from the "books" array above.
+const resolvers = {
+    Query: {
+      books: () => books,
+    },
+  };
+  
   
